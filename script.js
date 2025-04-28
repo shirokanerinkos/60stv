3ebe1c85053b4a6e75b9e1d4ba0fe5a4d5d372d2
shirokanerinkos <caitlynchiang0211@gmail.com>
2025-04-28 12:59:20 -0400

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,20 @@ function finishVoting() {
     channelDisplay.innerText = `Winner: ${channelInfo.text} (${highestVotes} votes)`;
   }, 3000); // 3000 milliseconds = 3 seconds
 }
+const votes = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0
+};
+
+const channels = {
+  1: { background: '#7f00ff', text: 'Channel 1' },
+  2: { background: '#ff4c4c', text: 'Channel 2' },
+  3: { background: '#00bfff', text: 'Channel 3' },
+  4: { background: '#00ff7f', text: 'Channel 4' },
+  5: { background: '#ffff00', text: 'Channel 5' }, // New Channel 5 (yellow)
+  6: { background: '#ff69b4', text: 'Channel 6' }  // New Channel 6 (pink)
+};
