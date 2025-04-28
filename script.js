cc51414bf7c54586f8590c141648596e249951ce
shirokanerinkos <caitlynchiang0211@gmail.com>
2025-04-28 12:32:49 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Retro TV Controller</title>
-  <link rel="stylesheet" href="style.css">
-</head>
-<body>
-  <div class="tv-screen" id="tvScreen">
-    <p id="channelDisplay">TV OFF</p>
-  </div>
+const powerButton = document.getElementById('powerButton');
+const channelUp = document.getElementById('channelUp');
+const channelDown = document.getElementById('channelDown');
+const tvScreen = document.getElementById('tvScreen');
+const channelDisplay = document.getElementById('channelDisplay');
 
-  <div class="remote-control">
-    <button id="powerButton" class="btn power">Power</button>
-    <button id="channelUp" class="btn">Channel +</button>
-    <button id="channelDown" class="btn">Channel -</button>
-  </div>
+let powerOn = false;
+let channel = 1;
 
-  <script src="script.js"></script>
-</body>
-</html>
+const channels = [
+  { background: 'black', text: 'TV OFF' },
+  { background: '#7f00ff', text: 'Channel 1' },
+  { background: '#ff4c4c', text: 'Channel 2' },
+  { background: '#00bfff', text: 'Channel 3' },
+  { background: '#00ff7f', text: 'Channel 4' }
+];
+
+function updateScreen() {
+  if (!powerOn) {
+    tvScreen.style.backgroundColor = channels[0].background;
+    channelDisplay.innerText = channels[0].text;
+  } else {
+    const currentChannel = channels[channel] || channels[1];
+    tvScreen.style.backgroundColor = currentChannel.background;
+    channelDisplay.innerText = currentChannel.text;
+  }
+}
+
+powerButton.addEventListener('click', () => {
+  powerOn = !powerOn;
+  updateScreen();
+});
+
+channelUp.addEventListener('click', () => {
+  if (powerOn) {
+    channel++;
+    if (channel >= channels.length) channel = 1;
+    updateScreen();
+  }
+});
+
+channelDown.addEventListener('click', () => {
+  if (powerOn) {
+    channel--;
+    if (channel < 1) channel = channels.length - 1;
+    updateScreen();
+  }
+});
+
+// Start with TV off
+updateScreen();
